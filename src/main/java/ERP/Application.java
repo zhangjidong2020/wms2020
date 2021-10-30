3514bbba395ae9a5d70ec2331fbcb13a6288b433
zhangjidong2020 <45511271+zhangjidong2020@users.noreply.github.com>
2021-10-30 20:46:51 +0800

    Update Application.java

--- src/main/java/ERP/Application.java
+++ src/main/java/ERP/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("ERP.mapper")
 public class Application {
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(Application.class,args);
     }
 }
